4fd3a3871cc1d92075004d7b06b0745b9f1f8ded
yixua <yixua@YIXUANS_LAPTOP>
2026-09-14 15:15:24 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// Variablen für die Tabelle "Mitarbeiter"
+int laufendeNummer = 12;                      // fortlaufende Nummer
+string mitarbeiterName = "Maier Hans";      // vollständiger Name
+int alter = 38;                               // Alter in Jahren
+string adresse = "Mühlenweg 1A, 01069 Dresden"; // Postanschrift
+string telefonnummer = "0177-1234567";      // Telefonnummer als String (führt Nullen/Trennzeichen)
+string beruf = "Fachinformatiker";          // Berufsbezeichnung
+bool verheiratet = true;                      // Familienstand: verheiratet
+bool lohnsteuerpflichtig = true;             // Lohnsteuerpflicht vorhanden (Ja/Nein)
+int lohnsteuerklasse = 3;                     // Lohnsteuerklasse (z.B. 3)
+int anzahlKinder = 2;                         // Anzahl der Kinder
+decimal gehalt = 2571.36m;                    // Monatsgehalt (Dezimal, m-Suffix)
+
+//2a ja
+//2b ja
+//2c nein
+//2d nein
+//2e nein
+//2f ja
+//2g ja
